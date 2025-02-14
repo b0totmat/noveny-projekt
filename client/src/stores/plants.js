05b71fd32797d109805bda0ed496fbef2bbd2402
Toth-Mate <t0thm4t3@gmail.com>
2025-02-14 12:57:42 +0100

    Pinia store a növényeknek

---
+++ client/src/stores/plants.js
@@ -0,0 +1,45 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+import PlantsService from '@/services/PlantsService'
+
+export const usePlantStore = defineStore('plants', () => {
+  const plants = ref([])
+
+  async function loadPlants() {
+    try {
+      plants.value = (await PlantsService.index()).data
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  async function newPlant(plant) {
+    try {
+      await PlantsService.insert(plant)
+      await loadPlants()
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  async function updatePlant(plant) {
+    try {
+      await PlantsService.update(plant)
+      await loadPlants()
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  async function removePlant(plantId) {
+    try {
+      await PlantsService.delete(plantId)
+      await loadPlants()
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  return { loadPlants, newPlant, updatePlant, removePlant }
+})
