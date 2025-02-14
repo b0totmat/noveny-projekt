de0abe5fb00785bf59c4cbf67284ea06a455e707
Toth-Mate <t0thm4t3@gmail.com>
2025-02-14 12:52:06 +0100

    Linkek javítása (style)

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -4,17 +4,17 @@ import { RouterLink } from 'vue-router'
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <RouterLink class="navbar-brand" to="/">Növény App</RouterLink>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <RouterLink to="/">Növények</RouterLink>
+                        <RouterLink to="/" class="nav-link">Növények</RouterLink>
                     </li>
                     <li class="nav-item">
-                        <RouterLink to="/new-plant">Új növény</RouterLink>
+                        <RouterLink to="/new-plant" class="nav-link">Új növény</RouterLink>
                     </li>
                 </ul>
             </div>
