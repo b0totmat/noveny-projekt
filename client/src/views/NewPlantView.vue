1a16604e659dd7bb0e4c8c31a4190b20bd51a694
b0totmat <t0thm4t3@gmail.com>
2025-02-14 17:11:59 +0100

    Új növény hozzáadása és átirányítás

--- client/src/views/NewPlantView.vue
+++ client/src/views/NewPlantView.vue
@@ -1,32 +1,32 @@
 <template>
     <main class="container">
     <div class="row gy-3">
-      <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+      <div class="col-12 col-md-6 col-lg-4 mx-auto border p-3">
         <form>
             <div class="form-floating mb-2">
-                <input type="text" class="form-control" id="plant-name" placeholder="Tulipán">
+                <input type="text" class="form-control" id="plant-name" placeholder="Tulipán" v-model="plant.name">
                 <label for="plant-name" class="form-label">Növény neve</label>
             </div>
             <div class="form-check mb-2">
                 <label for="is-perennial-yes" class="form-check-label">Évelő</label>
-                <input type="radio" name="is-perennial" id="is-perennial-yes" class="form-check-input">
+                <input type="radio" id="is-perennial-yes" :value="true" class="form-check-input" v-model="plant.isPerennial">
             </div>
             <div class="form-check mb-2">
                 <label for="is-perennial-no" class="form-check-label">Nem évelő</label>
-                <input type="radio" name="is-perennial" id="is-perennial-no" class="form-check-input">
+                <input type="radio"id="is-perennial-no" :value="false" class="form-check-input" v-model="plant.isPerennial">
             </div>
             <div class="mb-2">
-                <select class="form-select">
+                <select class="form-select" v-model="plant.type">
                     <option value="virág">Virág</option>
                     <option value="fa">Fa</option>
                     <option value="bokor">Bokor</option>
                 </select>
             </div>
-            <div class="form-floating mb-2">
-                <input type="number" id="price" class="form-control" placeholder="2500">
+            <div class="form-floating mb-3">
+                <input type="number" id="price" class="form-control" placeholder="2500" v-model="plant.price">
                 <label for="price" class="form-label">Ár</label>
             </div>
-            <button class="btn btn-success">Hozzáadás</button>
+            <button class="btn btn-success w-100" @click="newPlant">Hozzáadás</button>
         </form>
       </div>
     </div>
@@ -34,9 +34,24 @@
 </template>
 <script setup>
 import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { usePlantStore } from '@/stores/plants'
 
 const plantStore = usePlantStore()
-const route = useRoute()
+const route = useRoute(),
+      router = useRouter()
+
+const plant = ref({
+    name: '',
+    isPerennial: false,
+    type: 'virág',
+    price: 1000
+})
+
+async function newPlant(e) {
+    e.preventDefault()
+    
+    await plantStore.newPlant(plant.value)
+    router.push({ name: 'home' })
+}
 </script>
